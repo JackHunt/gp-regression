6d9fe38d82d5bf6e0612b418d3fed0b2f32bc83c
Jack Hunt <jackmileshunt@gmail.com>
2024-08-07 21:28:34 +0100

    Update make_pos_def.m

--- make_pos_def.m
+++ make_pos_def.m
@@ -1,18 +1,18 @@
 function M2 = make_pos_def(M)
 %MAKEPOSDEF Takes a matrix and makes it positive definite. 
-    [eigVal eigVec] = eig(M);
-    minVal = min(diag(eigVec));
+    [eig_val eig_vec] = eig(M);
+    minVal = min(diag(eig_vec));
     
-    for elem = 1 : size(eigVec, 1)
-        if eigVec(elem, elem) < 10^-10
-            eigVec(elem, elem) = eigVec(elem, elem)+2*abs(minVal);
+    for elem = 1 : size(eig_vec, 1)
+        if eig_vec(elem, elem) < 10^-10
+            eig_vec(elem, elem) = eig_vec(elem, elem)+2*abs(minVal);
         else
-            if eigVec(elem, elem) > 10^-10 && eigVec(elem, elem) < 0.00001
-                eigVec(elem, elem) = eigVec(elem, elem) + abs(minVal);
+            if eig_vec(elem, elem) > 10^-10 && eig_vec(elem, elem) < 0.00001
+                eig_vec(elem, elem) = eig_vec(elem, elem) + abs(minVal);
             end
         end
     end
     
-    M2 = eigVal * eigVec * eigVal';
+    M2 = eig_val * eig_vec * eig_val';
 end
 
