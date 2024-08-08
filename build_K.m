3d308e100e9a04282f39b72c0d60cf5d2a84654d
Jack Hunt <jackmileshunt@gmail.com>
2024-08-08 15:26:11 +0100

    Vectorise

--- build_K.m
+++ build_K.m
@@ -1,14 +1,18 @@
 function K = build_K(M, M2, theta, kernel)
 %GETCOV Returns the covarince matrix of 2 given matrices.
-    for i = 1 : size(M, 1)
-        for j = 1 : size(M2, 1)
-            if strcmp(kernel, 'sqExp')%Squared exponential.
-                K(i, j) = sq_exp(M(i, 1), M2(j, 1), theta, '');
-            elseif strcmp(kernel, '1o2')
-                K(i, j) = matern(M(i, 1), M2(j, 1), theta(1), theta(2), '1o2');
-            elseif strcmp(kernel, '3o2')
-                K(i, j) = matern(M(i, 1), M2(j, 1), theta(1), theta(2), '3o2');
-            end
-        end
+    m = size(M, 1);
+    n = size(M2, 1);
+
+    M_rep = repmat(M(:, 1), 1, n);
+    M2_rep = repmat(M2(:, 1)', m, 1);
+
+    if strcmp(kernel, 'sqExp')
+        K = sq_exp(M_rep, M2_rep, theta, '');
+    elseif strcmp(kernel, '1o2')
+        K = matern(M_rep, M2_rep, theta(1), theta(2), '1o2');
+    elseif strcmp(kernel, '3o2')
+        K = matern(M_rep, M2_rep, theta(1), theta(2), '3o2');
+    else
+        error('Unknown kernel type.');
     end
 end
--- kernels/sq_exp.m
+++ kernels/sq_exp.m
@@ -5,16 +5,16 @@ function y = sq_exp(x1, x2, theta, deriv)
     elseif (strcmp('lambda', deriv))
         y = d_lambda(x1, x2, theta(1), theta(2));
     else
-        y = (theta(2)^2) * exp(-((x1 - x2)^2) / (2 * theta(1)^2));
+        y = (theta(2).^2) * exp(-((x1 - x2).^2) / (2 * theta(1).^2));
     end
 end
 
 function y = d_sigma(x1, x2, lambda, sigma)
 %D_SIG Partial derivative w.r.t sigma.
-    y = (2 * sigma) * exp(-((x1 - x2)^2) / (2 * lambda^2));
+    y = (2 * sigma) * exp(-((x1 - x2).^2) / (2 * lambda.^2));
 end
 
 function y = d_lambda(x1, x2, lambda, sigma)
 %D_LAMBDA Partial derivative w.r.t lambda.
-    y = ((sigma^2 * (x1 - x2)^2) * exp(-((x1 - x2)^2)) / 2 * lambda^2) / lambda^3;
+    y = ((sigma.^2 * (x1 - x2).^2) * exp(-((x1 - x2).^2)) / 2 * lambda.^2) / lambda.^3;
 end
