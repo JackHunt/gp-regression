65de3e50d263374cec1ab140f3bb330a732412ab
Jack Hunt <jackmileshunt@gmail.com>
2024-08-08 15:34:31 +0100

    vectorise

--- get_sotonmet.m
+++ get_sotonmet.m
@@ -9,36 +9,25 @@ function [X, Y, X_s, Y_s] = get_sotonmet()
     sotonmet = readcell(fname);
     sotonmet = sotonmet(2:end, :);
 
-    for row = 1 : size(sotonmet, 1)
-        sotonmet{row, 1} = datenum(sotonmet{row, 1});
-        sotonmet{row, 3} = datenum(sotonmet{row, 3});
-    end
+    sotonmet(:, [1, 3]) = cellfun(@datenum, sotonmet(:, [1, 3]), 'UniformOutput', false);
 
     sotonmet(cellfun(@(x) all(ismissing(x)), sotonmet)) = {NaN};
-
-    to_delete = [];
-    X_s = [];
-    for row = 1 : size(sotonmet,1)
-        for col = 1 : size(sotonmet, 2)
-            if isnan(sotonmet{row, col})
-                X_s = [X_s; sotonmet{row, :}];
-                to_delete = [to_delete; row];
-                break
-            end
-        end
-    end
-
-    sotonmet(to_delete, :) = [];
-    X = cell2mat(sotonmet);
+    nan_rows = any(cellfun(@(x) isnan(x), sotonmet), 2);
     
+    % Extract rows with NaNs
+    X_s = sotonmet(nan_rows, :);
+    Y_s = cell2mat(X_s(:, LABEL_COL));
+    X_s = cell2mat(X_s(:, TO_KEEP));
+
+    % Remove rows with NaNs
+    sotonmet(nan_rows, :) = [];
+
+    % Convert remaining cell array to matrix
+    X = cell2mat(sotonmet);
     Y = X(:, Y_COL);
     X = X(:, TO_KEEP);
-    
-    Y_s = X_s(:, LABEL_COL);
-    X_s = X_s(:, TO_KEEP);
 
-    % Subtract first date from all dates.
+    % Subtract first date from all dates
     X = X - X(1,1);
     X_s = X_s - X_s(1,1);
-end
 
