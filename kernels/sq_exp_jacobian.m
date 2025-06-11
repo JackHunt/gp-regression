8b8753960f555dc440087b0817697d9d3f707611
Jack Hunt <jackmileshunt@gmail.com>
2025-06-11 23:03:51 +0100

    Update sq_exp_jacobian.m

--- kernels/sq_exp_jacobian.m
+++ kernels/sq_exp_jacobian.m
@@ -5,8 +5,8 @@ function J = sq_exp_jacobian(x1, x2, theta)
     
     sq_diff = (x1 - x2).^2;
 
-    d_l = (sigma.^2 * sq_diff * exp(-sq_diff) / 2 * lambda.^2) / lambda.^3;
-    d_s = 2 * sigma * exp(-sq_diff / (2 * lambda.^2));
+    d_l = (exp(-sq_diff / 2 * lambda^2) * sigma^2 * sq_diff) / lambda^3;
+    d_s = 2 * exp(-sq_diff / 2 * lambda^2) * sigma;
 
     J = {d_l; d_s};
 end
