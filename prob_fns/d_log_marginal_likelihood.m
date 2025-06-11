9f759b1c098ea628a109aac8c153625eefe22afd
Jack Hunt <jackmileshunt@gmail.com>
2025-06-11 23:03:54 +0100

    Update d_log_marginal_likelihood.m

--- prob_fns/d_log_marginal_likelihood.m
+++ prob_fns/d_log_marginal_likelihood.m
@@ -1,12 +1,12 @@
 function J = d_log_marginal_likelihood(K, Y, J_K)
 %D_LOG_MARGINAL_LIKELIHOOD Derivative of LML of a GP
     alpha = cholsolve(K, Y);
-    aTa = alpha * alpha';
+    aaT = alpha * alpha';
 
     J = [];
     for i = 1 : size(J_K, 1)
         J_K2 = cell2mat(J_K(i));
-        J = [J; 0.5 * trace(aTa * J_K2 - cholsolve(K, J_K2))];
+        J = [J; 0.5 * trace(aaT * J_K2 - cholsolve(K, J_K2))];
     end
 end
 
